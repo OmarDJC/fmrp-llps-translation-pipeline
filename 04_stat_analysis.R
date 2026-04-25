ef2402cb5ee3ff3bb7473d16592d34f68439841d
OmarDJC <omarjuradocarrera@gmail.com>
2026-04-25 13:15:30 -0600

    Update 04_stat_analysis.R

--- 04_stat_analysis.R
+++ 04_stat_analysis.R
@@ -1,18 +1,15 @@
 # Main statistical analysis method = "holm"
 
-library(ggplot2)
 library(EnvStats)
-library(ggstatsplot)
 library(scales)
 library(rstatix)
-library(scales)
 
 data <- read.csv("results/llps_scores_DecreasedTE_VS_IncreasedTE.csv", header=TRUE, sep=",")
 my_comparisons <- list(c("All proteins", "Decreased TE"),c("All proteins", "Increased TE"),c("Decreased TE", "Increased TE"))
 
 # Compare LLPS scores
 stat.test <- data %>%
-  t_test(CDS ~ group, paired = FALSE,comparisons = my_comparisons) %>%
+  t_test(PSPre_Score ~ group, paired = FALSE,comparisons = my_comparisons) %>%
   adjust_pvalue(method = "holm") %>%
   add_significance("p.adj")%>%
 stat.test1
