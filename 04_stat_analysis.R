58e904f55e8b452482e632d997cef4558b12a547
OmarDJC <omarjuradocarrera@gmail.com>
2026-04-26 12:07:36 -0600

    04_stat_analysis.R

--- 04_stat_analysis.R
+++ 04_stat_analysis.R
@@ -1,9 +1,11 @@
-# Main statistical analysis method = "holm"
+# Main statistical analysis method t_test "holm"
+#Apply Holm correction to control family-wise error rate
 
+#Load required libraries
 library(EnvStats)
-library(scales)
 library(rstatix)
 
+#Import datasets
 data <- read.csv("results/llps_scores_DecreasedTE_VS_IncreasedTE.csv", header=TRUE, sep=",")
 my_comparisons <- list(c("All proteins", "Decreased TE"),c("All proteins", "Increased TE"),c("Decreased TE", "Increased TE"))
 
